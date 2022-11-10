333966ab504eca809a22916bf6adebb924717ae4
Toky Chanthavong <tchantha01@gmail.com>
2022-11-09 17:55:03 -0700

    making relationships

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 """Models for movie ratings app."""
-import os
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -11,11 +11,11 @@ class User(db.Model):
     __tablename__ = 'users'
     
     user_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    email = db.Column(db.String(255), nullable = False, unique = True)
-    password = db.Column(db.String(255), nullable = False)
+    email = db.Column(db.String(255), unique = True)
+    password = db.Column(db.String(255))
     
     def __repr__(self):
-        return f'<User user_id={self.user_id} email = {self.email}>'
+        return f"<User user_id={self.user_id} email = {self.email}>"
     
 class Movie(db.Model):
     
@@ -24,15 +24,37 @@ class Movie(db.Model):
     movie_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String(225))   
     overview = db.Column(db.Text) 
+    release_date = db.Column(db.DateTime) 
+    poster_path = db.Column(db.String(255)) 
+    
+    def __repr__(self):
+        return f"<Movie movie_id={self.movie_id} title={self.title}>"
+    
+class Rating(db.Model):
+    
+    __tablename__ = 'ratings'
+    
+    rating_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))  
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))  
+    
+    movie = db.relationship("Movie", backref="ratings")
+    user = db.relationship("User", backref="ratings")
+    
+    def __repr__(self):
+        return f"<Rating rating_id={self.rating_id} score={self.score}>"
+    
+
     
     
 
 
-def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
-    flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['db_uri']
+def connect_to_db(flask_app, db_uri=os.environ["POSTGRES_URI"], echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+ 
     db.app = flask_app
     db.init_app(flask_app)
 
@@ -46,4 +68,4 @@ if __name__ == "__main__":
     # too annoying; this will tell SQLAlchemy not to print out every
     # query it executes.
 
-    connect_to_db(app)
+    connect_to_db(app, echo=False)
