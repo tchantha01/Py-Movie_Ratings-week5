73e752d45aa331cc4cbdb85a2dbc66bde4f8a0ee
Toky Chanthavong <tchantha01@gmail.com>
2022-11-10 23:02:03 -0700

    created user(s)/movie(s) details for server, crud, and html

--- crud.py
+++ crud.py
@@ -16,6 +16,26 @@ def create_movie(title, overview, release_date, poster_path):
     
     return movie
 
+def get_users():
+    """Get all users"""
+    
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Get user by their id"""
+    
+    return User.query.get(user_id)
+
+def get_movies():
+    """Return all movies"""
+    
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Get a movie by id"""
+    
+    return Movie.query.get(movie_id)
+
 def create_rating(user, movie, score):
     """Create and return a new rating"""
     
--- seed_database.py
+++ seed_database.py
@@ -15,7 +15,7 @@ os.system("dropdb ratings")
 os.system("createdb ratings")
 
 #connect to the database and call db.create_all()
-model.connect_to_db(server.app)
+model.connect_to_db(server.app, echo=False)
 model.db.create_all()
 
 #load data from json file and set to a variable
@@ -34,6 +34,25 @@ for movie in movie_data:
     release_date = datetime.strptime(movie["release_date"], "%Y-%m-%d")
     
     db_movie = crud.create_movie(title, overview, release_date, poster_path)
+    movies_in_db.append(db_movie)
     
 model.db.session.add_all(movies_in_db)
-model.db.session.commit()    
+model.db.session.commit()  
+
+
+#randomly generate 10 users; each user will make 10 ratings
+for n in range(10):
+    email = f'user{n}@test.com'
+    password = 'test'
+    
+    user = crud.create_user(email, password)
+    model.db.session.add(user)
+    
+    for _ in range(10):
+        random_movie = choice(movies_in_db)
+        score = randint(1, 5) 
+        
+        rating = crud.create_rating(user, random_movie, score)
+        model.db.session.add(rating)
+        
+model.db.session.commit()         
